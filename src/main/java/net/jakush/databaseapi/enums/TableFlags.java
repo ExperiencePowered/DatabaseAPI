fa89c1928e983f1fea274e4983f3870a4cc791ea
Jakub Šulc <James_Sulc@outlook.cz>
2023-08-01 13:20:08 +0200

    Changed CREATE_IF_NOT_EXISTS from enum TableFlags to IF_NOT_EXISTS

--- src/main/java/net/jakush/databaseapi/enums/TableFlags.java
+++ src/main/java/net/jakush/databaseapi/enums/TableFlags.java
@@ -4,11 +4,11 @@ package net.jakush.databaseapi.enums;
  * This file is a part of DatabaseAPI <br>
  * Author: <a href="https://github.com/Jakush">Jakush</a><br>
  * <br>
- * Flags when creating table as {@link TableFlags#CREATE_IF_NOT_EXISTS}
+ * Flags when creating table as {@link TableFlags#IF_NOT_EXISTS}
  */
 public enum TableFlags {
 
-    CREATE_IF_NOT_EXISTS
+    IF_NOT_EXISTS
     ;
 
 }
